4de3754da59774b0d28c59867644ac5092620ac5
HarshithKatari <164130354+HarshithKatari@users.noreply.github.com>
2024-06-22 13:16:11 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,92 @@
+const biriyani=document.querySelector(".biriyani");
+biriyani.addEventListener("click",()=>{
+    window.location.href="https://www.swiggy.com/collections/83649?collection_id=83649&search_context=biryani&tags=layout_CCS_Biryani&type=rcv2"
+})
+const burger=document.querySelector(".burger");
+burger.addEventListener("click",()=>{
+    window.location.href="https://www.swiggy.com/collections/83637?collection_id=83637&search_context=burger&tags=layout_CCS_Burger&type=rcv2"
+})
+const pizza=document.querySelector(".pizza");
+pizza.addEventListener("click",()=>{
+    window.location.href="https://www.swiggy.com/collections/83644?collection_id=83644&search_context=pizza&tags=layout_CCS_Pizza&type=rcv2"
+});
+const shawarma=document.querySelector(".shawarma");
+shawarma.addEventListener("click",()=>{
+    window.location.href="https://www.swiggy.com/collections/80403?collection_id=80403&tags=layout_Shawarma_Contextual&type=rcv2";
+});
+const icecream=document.querySelector(".icecreams");
+icecream.addEventListener("click",()=>{
+    window.location.href="https://www.swiggy.com/collections/83650?collection_id=83650&tags=layout_CCS_IceCreams&type=rcv2";
+});
+const idli=document.querySelector(".idli");
+idli.addEventListener("click",()=>{
+    window.location.href="https://www.swiggy.com/collections/80441?collection_id=80441&searchContext=idli&tags=layout_CCS_Idli&type=rcv2";
+});
+const restaurant1=document.querySelector(".restaurant1");
+restaurant1.addEventListener("click",()=>{
+    window.location.href="https://www.swiggy.com/restaurants/ubq-by-barbeque-nation-stv-nagar-tirumala-tirupati-128159";
+})
+const restaurant2=document.querySelector(".restaurant2");
+restaurant2.addEventListener("click",()=>{
+    window.location.href="https://www.swiggy.com/restaurants/mcdonalds-stv-nagar-tirumala-tirupati-131588";
+})
+const restaurant3=document.querySelector(".restaurant3");
+restaurant3.addEventListener("click",()=>{
+    window.location.href="https://www.swiggy.com/restaurants/sree-dhruv-mithai-vatika-peddakapu-layout-tirumala-tirupati-613691";
+})
+const restaurant4=document.querySelector(".restaurant4");
+restaurant4.addEventListener("click",()=>{
+    window.location.href="https://www.swiggy.com/restaurants/biryaniwala-laxmipuram-circle-thanapalli-road-tirupati-338323";
+})
+const restaurant5=document.querySelector(".restaurant5");
+restaurant5.addEventListener("click",()=>{
+    window.location.href="https://www.swiggy.com/restaurants/deforest-restaurant-mr-pally-dhanalaxmi-nagar-tirupati-80072";
+})
+const restaurant6=document.querySelector(".restaurant6");
+restaurant6.addEventListener("click",()=>{
+    window.location.href="https://www.swiggy.com/restaurants/baskin-robbins-ice-cream-desserts-tiruchnoor-road-korramenugunta-tirupati-99522";
+})
+const restaurant7=document.querySelector(".restaurant7");
+restaurant7.addEventListener("click",()=>{
+    window.location.href="https://www.swiggy.com/restaurants/lasa-food-court-tiruchinoor-road-padmavathi-nagar-tirupati-108117";
+})
+const restaurant8=document.querySelector(".restaurant8");
+restaurant8.addEventListener("click",()=>{
+    window.location.href="https://www.swiggy.com/restaurants/tabun-an-arabic-restaurant-mango-market-road-opp-to-taj-tirupati-111201";
+})
+const restaurant9=document.querySelector(".restaurant9");
+restaurant9.addEventListener("click",()=>{
+    window.location.href="https://www.swiggy.com/restaurants/sri-annapurna-mess-and-catring-services-lakshmi-puram-circle-lakshmipuram-circle-tirupati-419785";
+})
+const restaurant10=document.querySelector(".restaurant10");
+restaurant10.addEventListener("click",()=>{
+    window.location.href="https://www.swiggy.com/restaurants/kfc-vv-mahal-road-korlagunta-tirupati-84467";
+})
+const restaurant11=document.querySelector(".restaurant11");
+restaurant11.addEventListener("click",()=>{
+    window.location.href="https://www.swiggy.com/restaurants/cream-stone-ice-cream-puthalapathu-naidupeta-road-tata-nagar-tirupati-140581";
+});
+const restaurant12=document.querySelector(".restaurant12");
+restaurant12.addEventListener("click",()=>{
+    window.location.href="https://www.swiggy.com/restaurants/kwality-walls-frozen-dessert-and-ice-cream-shop-renigunta-road-korramenugunta-tirupati-720033";
+});
+const restaurant13=document.querySelector(".restaurant13");
+restaurant13.addEventListener("click",()=>{
+    window.location.href="https://www.swiggy.com/restaurants/scoops-ice-cream-korlagunta-tirupati-824778";
+});
+const restaurant14=document.querySelector(".restaurant14");
+restaurant14.addEventListener("click",()=>{
+    window.location.href="https://www.swiggy.com/restaurants/ibaco-air-bypass-road-tirumala-tirupati-80806";
+});
+const restaurant15=document.querySelector(".restaurant15");
+restaurant15.addEventListener("click",()=>{
+    window.location.href="https://www.swiggy.com/restaurants/perambur-sri-srinivasa-sweets-pure-veg-restaurant-new-tiruchanoor-road-tirupati-593313";
+});
+const restaurant16=document.querySelector(".restaurant16");
+restaurant16.addEventListener("click",()=>{
+    window.location.href="https://www.swiggy.com/restaurants/hotel-orion-sivajyothi-nagar-tirumala-bypass-road-tirupati-600877";
+});
+
+
+
+
---
+++ index1.js
@@ -0,0 +1,64 @@
+const restaurant1=document.querySelector(".restaurant1");
+restaurant1.addEventListener("click",()=>{
+    window.location.href="https://www.swiggy.com/restaurants/ubq-by-barbeque-nation-stv-nagar-tirumala-tirupati-128159";
+})
+const restaurant2=document.querySelector(".restaurant2");
+restaurant2.addEventListener("click",()=>{
+    window.location.href="https://www.swiggy.com/restaurants/mcdonalds-stv-nagar-tirumala-tirupati-131588";
+})
+const restaurant3=document.querySelector(".restaurant3");
+restaurant3.addEventListener("click",()=>{
+    window.location.href="https://www.swiggy.com/restaurants/sree-dhruv-mithai-vatika-peddakapu-layout-tirumala-tirupati-613691";
+})
+const restaurant4=document.querySelector(".restaurant4");
+restaurant4.addEventListener("click",()=>{
+    window.location.href="https://www.swiggy.com/restaurants/biryaniwala-laxmipuram-circle-thanapalli-road-tirupati-338323";
+})
+const restaurant5=document.querySelector(".restaurant5");
+restaurant5.addEventListener("click",()=>{
+    window.location.href="https://www.swiggy.com/restaurants/deforest-restaurant-mr-pally-dhanalaxmi-nagar-tirupati-80072";
+})
+const restaurant6=document.querySelector(".restaurant6");
+restaurant6.addEventListener("click",()=>{
+    window.location.href="https://www.swiggy.com/restaurants/baskin-robbins-ice-cream-desserts-tiruchnoor-road-korramenugunta-tirupati-99522";
+})
+const restaurant7=document.querySelector(".restaurant7");
+restaurant7.addEventListener("click",()=>{
+    window.location.href="https://www.swiggy.com/restaurants/lasa-food-court-tiruchinoor-road-padmavathi-nagar-tirupati-108117";
+})
+const restaurant8=document.querySelector(".restaurant8");
+restaurant8.addEventListener("click",()=>{
+    window.location.href="https://www.swiggy.com/restaurants/tabun-an-arabic-restaurant-mango-market-road-opp-to-taj-tirupati-111201";
+})
+const restaurant9=document.querySelector(".restaurant9");
+restaurant9.addEventListener("click",()=>{
+    window.location.href="https://www.swiggy.com/restaurants/sri-annapurna-mess-and-catring-services-lakshmi-puram-circle-lakshmipuram-circle-tirupati-419785";
+})
+const restaurant10=document.querySelector(".restaurant10");
+restaurant10.addEventListener("click",()=>{
+    window.location.href="https://www.swiggy.com/restaurants/kfc-vv-mahal-road-korlagunta-tirupati-84467";
+})
+const restaurant11=document.querySelector(".restaurant11");
+restaurant11.addEventListener("click",()=>{
+    window.location.href="https://www.swiggy.com/restaurants/cream-stone-ice-cream-puthalapathu-naidupeta-road-tata-nagar-tirupati-140581";
+});
+const restaurant12=document.querySelector(".restaurant12");
+restaurant12.addEventListener("click",()=>{
+    window.location.href="https://www.swiggy.com/restaurants/kwality-walls-frozen-dessert-and-ice-cream-shop-renigunta-road-korramenugunta-tirupati-720033";
+});
+const restaurant13=document.querySelector(".restaurant13");
+restaurant13.addEventListener("click",()=>{
+    window.location.href="https://www.swiggy.com/restaurants/scoops-ice-cream-korlagunta-tirupati-824778";
+});
+const restaurant14=document.querySelector(".restaurant14");
+restaurant14.addEventListener("click",()=>{
+    window.location.href="https://www.swiggy.com/restaurants/ibaco-air-bypass-road-tirumala-tirupati-80806";
+});
+const restaurant15=document.querySelector(".restaurant15");
+restaurant15.addEventListener("click",()=>{
+    window.location.href="https://www.swiggy.com/restaurants/perambur-sri-srinivasa-sweets-pure-veg-restaurant-new-tiruchanoor-road-tirupati-593313";
+});
+const restaurant16=document.querySelector(".restaurant16");
+restaurant16.addEventListener("click",()=>{
+    window.location.href="https://www.swiggy.com/restaurants/hotel-orion-sivajyothi-nagar-tirumala-bypass-road-tirupati-600877";
+});
